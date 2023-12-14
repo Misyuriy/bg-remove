784f21437cdea742ce5e556442e6a4b12a4c7bc4
Misyuriy <andrey.misyurev@yandex.ru>
2023-12-14 14:46:29 +0300

    Implemented file saving and preview images

--- interface.py
+++ interface.py
@@ -2,7 +2,9 @@ import tkinter
 from tkinter import ttk
 from tkinter import filedialog
 
+import numpy
 import cv2
+
 from bg_remover import BgRemover
 from PIL import Image, ImageTk
 
@@ -33,7 +35,7 @@ class BgRemoverWindow:
         return self.root
 
     def _on_image_load_pressed(self):
-        file_path = filedialog.askopenfilename(filetypes=[("High resolution image files", "*.png"), ("JPEG image files", "*.jpg"), ("All files", "*.*")])
+        file_path = filedialog.askopenfilename(title="Load image", filetypes=[("High resolution image files", "*.png"), ("JPEG image files", "*.jpg"), ("All files", "*.*")])
         if not file_path:
             return
 
@@ -42,14 +44,33 @@ class BgRemoverWindow:
         remover = BgRemover()
         removed_image = remover.remove(image)
 
-        photo_image = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(removed_image, cv2.COLOR_BGR2RGB)))
-
-        label = tkinter.Label(image=photo_image)
-        label.image = photo_image
-        label.pack(side=tkinter.TOP)
+        self.display_initial_preview(image)
+        self.display_result_preview(removed_image)
 
         self.result = removed_image
 
     def _on_image_save_pressed(self):
-        if not self.result:
+        if self.result is None:
             return
+
+        file_path = filedialog.asksaveasfilename(title="Save image as...", defaultextension=".png", filetypes=[("High resolution image files", "*.png")])
+        if file_path[-4:] != ".png":
+            file_path += ".png"
+
+        cv2.imwrite(filename=file_path, img=self.result)
+
+    def display_initial_preview(self, image: numpy.ndarray):
+        resized_image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)).resize(size=(192, 192))
+        photo_image = ImageTk.PhotoImage(image=resized_image)
+
+        label = tkinter.Label(image=photo_image)
+        label.image = photo_image
+        label.grid(row=2, column=1)
+
+    def display_result_preview(self, image: numpy.ndarray):
+        resized_image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)).resize(size=(192, 192))
+        photo_image = ImageTk.PhotoImage(image=resized_image)
+
+        label = tkinter.Label(image=photo_image)
+        label.image = photo_image
+        label.grid(row=2, column=3)
