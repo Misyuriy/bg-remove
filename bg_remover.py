7538b9db891e64f8addf56c712100bdb35a65c05
Misyuriy <andrey.misyurev@yandex.ru>
2023-12-17 14:47:48 +0300

    Added manual threshold adjustment, improved calibration through automatic image cropping

--- bg_remover.py
+++ bg_remover.py
@@ -3,15 +3,51 @@ import numpy
 
 
 class BgRemover:
-    def __init__(self, deviation_threshold: int = 0):
-        pass
+    def __init__(self, calibration_threshold: int = 241, calibration_average: float = 183.2592):
+        self.calibration_threshold = calibration_threshold
+        self.calibration_average = calibration_average
 
-    def remove(self, image: numpy.ndarray):
+    def remove(self, image: numpy.ndarray, manual_threshold: int = None):
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        _ret, threshold_image = cv2.threshold(gray_image, 245, 255, cv2.THRESH_BINARY)
+        if not manual_threshold:
+            boundaries_threshold = 254
+            _ret, boundaries_threshold_image = cv2.threshold(gray_image, boundaries_threshold, 255, cv2.THRESH_BINARY)
+
+            min_y, max_y, min_x, max_x = self.detect_boundaries(cv2.bitwise_not(boundaries_threshold_image))
+            threshold = self.calibration_threshold * (self.calibration_average / numpy.average(image[min_y: max_y, min_x: max_x]))
+
+        else:
+            threshold = manual_threshold
+
+        _ret, threshold_image = cv2.threshold(gray_image, threshold, 255, cv2.THRESH_BINARY)
 
         threshold_image = cv2.bitwise_not(threshold_image)
 
         result_image = cv2.bitwise_and(image, image, mask=threshold_image)
-        return result_image
+        return result_image, threshold
+
+    def detect_boundaries(self, image: numpy.ndarray):
+        contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        max_x = 0
+        max_y = 0
+        min_x = image.shape[0]
+        min_y = image.shape[1]
+
+        for contour in contours:
+            point = list(contour[0][0])
+
+            if point[0] < min_x:
+                min_x = point[0]
+            if point[1] < min_y:
+                min_y = point[1]
+
+            if point[0] > max_x:
+                max_x = point[0]
+            if point[1] > max_y:
+                max_y = point[1]
+
+        print((min_x, min_y), (max_x, max_y))
+
+        return min_y, max_y, min_x, max_x
--- interface.py
+++ interface.py
@@ -16,6 +16,7 @@ class BgRemoverWindow:
         self.geometry = list(map(int, geometry.split("x")))
         self.root.geometry(geometry)
 
+        self.current_image = None
         self.result = None
 
         tkinter.Label(width=8).grid(row=0, column=0)
@@ -23,13 +24,23 @@ class BgRemoverWindow:
         load_button = ttk.Button(text="Load file", command=self._on_image_load_pressed, width=32)
         load_button.grid(row=0, column=1)
 
-        tkinter.Label(width=24).grid(row=0, column=2)
+        tkinter.Label(width=24).grid(row=0, column=2, columnspan=3)
 
         save_button = ttk.Button(text="Save file", command=self._on_image_save_pressed, width=32)
-        save_button.grid(row=0, column=3)
+        save_button.grid(row=0, column=5)
 
         tkinter.Label(text="Initial image preview:").grid(row=1, column=1)
-        tkinter.Label(text="Result image preview:").grid(row=1, column=3)
+        tkinter.Label(text="Result image preview:").grid(row=1, column=5)
+
+        tkinter.Label(height=13).grid(row=2, column=0)
+
+        tkinter.Label(text="Pixel threshold:").grid(row=3, column=2, columnspan=3)
+
+        self.threshold_label = tkinter.Label(text="x")
+        self.threshold_label.grid(row=4, column=3)
+
+        ttk.Button(text="-", command=self.decrease_threshold).grid(row=4, column=2)
+        ttk.Button(text="+", command=self.increase_threshold).grid(row=4, column=4)
 
     def get_root(self) -> tkinter.Tk:
         return self.root
@@ -40,14 +51,9 @@ class BgRemoverWindow:
             return
 
         image = cv2.imread(file_path)
+        self.current_image = image
 
-        remover = BgRemover()
-        removed_image = remover.remove(image)
-
-        self.display_initial_preview(image)
-        self.display_result_preview(removed_image)
-
-        self.result = removed_image
+        self.result = self.remove_bg(image)
 
     def _on_image_save_pressed(self):
         if self.result is None:
@@ -58,6 +64,32 @@ class BgRemoverWindow:
             file_path += ".png"
 
         cv2.imwrite(filename=file_path, img=self.result)
+
+    def increase_threshold(self):
+        if self.threshold_label.cget("text") == "x":
+            return
+
+        threshold = int(self.threshold_label.cget("text"))
+        if threshold >= 255:
+            return
+
+        threshold += 1
+        self.threshold_label.config(text=round(threshold))
+
+        self.result = self.remove_bg(self.current_image, threshold)
+
+    def decrease_threshold(self):
+        if self.threshold_label.cget("text") == "x":
+            return
+
+        threshold = int(self.threshold_label.cget("text"))
+        if threshold <= 1:
+            return
+
+        threshold -= 1
+        self.threshold_label.config(text=round(threshold))
+
+        self.result = self.remove_bg(self.current_image, threshold)
 
     def display_initial_preview(self, image: numpy.ndarray):
         resized_image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)).resize(size=(192, 192))
@@ -73,4 +105,15 @@ class BgRemoverWindow:
 
         label = tkinter.Label(image=photo_image)
         label.image = photo_image
-        label.grid(row=2, column=3)
+        label.grid(row=2, column=5)
+
+    def remove_bg(self, image: numpy.ndarray, manual_threshold: int = None):
+        remover = BgRemover()
+        removed_image, threshold = remover.remove(image, manual_threshold)
+
+        self.threshold_label.config(text=round(threshold))
+
+        self.display_initial_preview(image)
+        self.display_result_preview(removed_image)
+
+        return removed_image
